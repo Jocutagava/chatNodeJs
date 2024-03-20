e0c8c9b8eb4796d5ff4bbdbb7d1e40172d54c3e3
Jocutagava <bratoliubkirill@gmail.com>
2024-03-20 15:53:26 +0100

    xknxnka

--- server.js
+++ server.js
@@ -42,6 +42,11 @@ server.listen(3000);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+
 io.on('connection', (socket)=>{
     console.log('a user connected. id - ' + socket.id);
+
+    socket.on('new_message', (message)=>{
+        io.emit('message', message)
+    })
 });
